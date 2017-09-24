d2265d7c785e64b6a08d352313bc0dd6f275931a
TrinhTran <salem02t1@gmail.com>
2017-09-25 00:13:24 +0700

    commit code

---
+++ onlineshopping/src/main/java/test/onlineshopping/controllers/PageController.java
@@ -0,0 +1,17 @@
+package test.onlineshopping.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value = {"/", "home"})
+	public ModelAndView index()
+	{
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", "Hello World");
+		return mv;
+	}
+}
